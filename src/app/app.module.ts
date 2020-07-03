a9be560120e0125dae84c970f813ccb4ce4a305f
Brett Mayen <Brett.Mayen@softrams.com>
2020-07-03 07:14:11 -0400

    add ngxs project example

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsModule } from '@ngxs/store';
+
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+const NGXS_IMPORTS = [
+  NgxsModule.forRoot([], {
+    developmentMode: !environment.production,
+    selectorOptions: {
+      // These Selector Settings are recommended in preparation for NGXS v4
+      // (See above for their effects)
+      suppressErrors: false,
+      injectContainerState: false
+    },
+    compatibility: {
+      strictContentSecurityPolicy: true
+    },
+  }),
+  NgxsRouterPluginModule.forRoot(),
+  NgxsReduxDevtoolsPluginModule.forRoot({
+    disabled: environment.production,
+  }),
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ...NGXS_IMPORTS,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
