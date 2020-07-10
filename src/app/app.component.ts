13d90c33deaeaf415900cf3f36f0f8a7cdeede32
Brett Mayen <Brett.Mayen@softrams.com>
2020-07-10 11:19:55 -0400

    add Count action to verify that other state is updated via DevTools as expected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { CountState, Add } from './app.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngxs';
+
+  @Select(CountState) count$: Observable<number>;
+
+  constructor(private store: Store) {}
+
+  onClick() {
+    this.store.dispatch(new Add());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
@@ -8,9 +9,10 @@ import { NgxsModule } from '@ngxs/store';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountState } from './app.state';
 
 const NGXS_IMPORTS = [
-  NgxsModule.forRoot([], {
+  NgxsModule.forRoot([CountState], {
     developmentMode: !environment.production,
     selectorOptions: {
       // These Selector Settings are recommended in preparation for NGXS v4
@@ -23,6 +25,10 @@ const NGXS_IMPORTS = [
     },
   }),
   NgxsRouterPluginModule.forRoot(),
+  NgxsLoggerPluginModule.forRoot({
+    collapsed: true,
+    disabled: environment.production,
+  }),
   NgxsReduxDevtoolsPluginModule.forRoot({
     disabled: environment.production,
   }),
